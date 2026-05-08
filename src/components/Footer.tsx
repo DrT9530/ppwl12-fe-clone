68274b62abd596f3036e5c0a498f900fb1080994
DrT9530 <athallahrisky@gmail.com>
2026-05-08 10:20:34 +0700

    feat: implement landing page components, shared header, and login page structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,10 +44,16 @@ export default function Footer() {
 
         <div className="mb-6">
           <div className="relative inline-block">
-            <select className="appearance-none bg-transparent border border-white/30 text-white/70 text-sm px-4 py-2 pr-10 rounded cursor-pointer hover:bg-white/10 transition-colors">
+            <select className="appearance-none bg-transparent border border-white/30 text-white/70 text-sm pl-10 pr-10 py-2 rounded cursor-pointer hover:bg-white/10 transition-colors">
               <option value="id" className="bg-black text-white">Bahasa Indonesia</option>
               <option value="en" className="bg-black text-white">English</option>
             </select>
+
+            <div className="absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none text-white">
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M10.77 5.33l-.27.67L9.34 8.94l-.57 1.44L7.33 14h1.78l1.73-1.97h3.58l1.74 1.97H16l-3.43-8.67zm-.15 4.6-.24.63h2.51l-1.26-3.35zm-1.1-5.09l.1-.19h-3.2V2h-1.5v2.65H.55V6h3.77A11 11 0 0 1 0 10.43c.33.28.81.8 1.05 1.16 1.5-.91 2.85-2.36 3.88-4.02v5.1h1.49V7.52q.6.95 1.33 1.81l.57-1.43a12 12 0 0 1-1.34-1.9h2.09z" fill="currentColor"></path>
+              </svg>
+            </div>
 
             <div className="absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none text-white">
               <svg 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,14 +32,18 @@ export default function Header() {
         <div className="header-lang-select">
           <svg
             className="header-lang-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
             fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <circle cx="12" cy="12" r="10" />
-            <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.77 5.33l-.27.67L9.34 8.94l-.57 1.44L7.33 14h1.78l1.73-1.97h3.58l1.74 1.97H16l-3.43-8.67zm-.15 4.6-.24.63h2.51l-1.26-3.35zm-1.1-5.09l.1-.19h-3.2V2h-1.5v2.65H.55V6h3.77A11 11 0 0 1 0 10.43c.33.28.81.8 1.05 1.16 1.5-.91 2.85-2.36 3.88-4.02v5.1h1.49V7.52q.6.95 1.33 1.81l.57-1.43a12 12 0 0 1-1.34-1.9h2.09z"
+              fill="currentColor"
+            />
           </svg>
           <select className="header-select" defaultValue="en">
             <option value="en" className="bg-black text-white">
